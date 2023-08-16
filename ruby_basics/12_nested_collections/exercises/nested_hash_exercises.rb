90e3f03ad5a52f903005b6b91982365725288912
Dawson Myers <Djmyers2002@gmail.com>
2023-08-16 12:54:09 -0400

    nested hash exercises road block

--- ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
@@ -20,22 +20,36 @@ end
 
 def add_language(languages, language_name, language_info_value)
   # Take languages and add the key/value pair language_name/language_info_value
+  languages[language_name] = language_info_value
   # to it, then return languages
+  languages
 end
 
 def delete_information_about_language(languages, language_name, info_key)
   # Take languages and delete the key/value pair with key info_key from
   # language_name, then return languages
+
+  languages[language_name].delete(info_key)
+
+  languages
+
 end
 
 def delete_language(languages, language_name)
   # Take languages and delete the language_name key/value pair, then return
   # languages
+
+  languages.delete(language_name)
+
+  languages
 end
 
 def find_beautiful_languages(languages)
   # Take languages and return a hash containing only languages which have the
   # key/value pair { is_beautiful?: true } listed in their information
+
+  languages.select { |key, value| value[:is_beautiful?] == true }
+
 end
 
 def find_language_facts(languages, language_name, fact_index = 0)
@@ -54,4 +68,6 @@ def find_language_facts(languages, language_name, fact_index = 0)
   #                 initial_release: 'December 4, 1995',
   #                 is_beautiful?: false }
   # }
+  language = languages[language_name]
+
 end
