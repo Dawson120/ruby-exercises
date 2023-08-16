b967b8051bb9fda1fb8aa3dbe87ef1d6e30aab8b
Dawson Myers <Djmyers2002@gmail.com>
2023-08-16 02:44:06 -0400

    method exercise start

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -13,6 +13,9 @@ end
 
 
 # method name: #common_sports
+def common_sports(current_sports, favorite_sports)
+    current_sports & favorite_sports
+end
 # parameters: current_sports and favorite_sports (both arrays)
 # return value: an array containing items in both arrays
 # hint: use Array#intersection
@@ -22,20 +25,32 @@ end
 # parameter: games (an array)
 # return value: games, alphabetically sorted and duplicates removed
 # hint: chain Array#sort and Array#uniq together
+def alphabetical_list(games)
+    sorted_games = games.sort.uniq
+    sorted_games
+end
 
 
 # method name: #lucky_number
 # parameter: number (an integer) with default value of 7
 # return value: a string "Today's lucky number is <number>"
-
+def lucky_number(number = 7)
+    "Today's lucky number is #{number}"
+end
 
 # method name: #ascii_code
 # parameter: character (a string)
 # return value: the character's ordinal number
 # explicit return value: 'Input Error' if character's length does not equal 1
 # hint: use String#ord
-
-
+def ascii_code(character)
+    if character.length != 1
+        return "Input Error"
+    end
+    ord_character = character.ord
+    ord_character
+end
+    
 # method name: #pet_pun
 # parameter: animal (a string)
 # return value: nil
@@ -44,9 +59,22 @@ end
 # console output: otherwise, "I think <animal>s have pet-tential!" (potential)
 # hint: use puts
 
+def pet_pun(animal)
+    if animal == "cat"
+        puts "Cats are purr-fect!"
+    elsif animal == "dog"
+        puts "Dogs are paw-some!"
+    else
+        puts "I think #{animal}s have pet-tential!"
+    end
+end
 
 # method name: #twenty_first_century?
 # parameter: year (an integer)
 # return value: true if the year is between 2001 - 2100, otherwise return false
 # hint: use Comparable#between?
 
+def twenty_first_century?(year)
+    year.between?(2001, 2100)
+end
+        
