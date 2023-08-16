e097e9339be8e051c0601f8a20a83ea8b5bed122
Dawson Myers <Djmyers2002@gmail.com>
2023-08-16 07:16:24 -0400

    enumerables exercise completed

--- ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
+++ ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
@@ -8,22 +8,31 @@ end
 
 def display_guess_order(guesses)
   # use #each_with_index to iterate through each item of the guesses (an array)
+  guesses.each_with_index do |guess, index|
   # use puts to output each list item "Guess #<number> is <item>" to console
+  puts "Guess ##{index + 1} is #{guess}"
+  end
   # hint: the number should start with 1
 end
 
 def find_absolute_values(numbers)
   # use #map to iterate through each item of the numbers (an array)
+  numbers.map { |number| number.abs}
   # return an array of absolute values of each number
 end
 
 def find_low_inventory(inventory_list)
   # use #select to iterate through each item of the inventory_list (a hash)
+  inventory_list.select { |key, value| value < 4 }
   # return a hash of items with values less than 4
 end
 
 def find_word_lengths(word_list)
   # use #reduce to iterate through each item of the word_list (an array)
+  word_list.reduce({}) do |lengths_hash, word|
+    lengths_hash[word] = word.length
   # return a hash with each word as the key and its length as the value
+    lengths_hash 
+  end
   # hint: look at the documentation and review the reduce examples in basic enumerable lesson
 end
